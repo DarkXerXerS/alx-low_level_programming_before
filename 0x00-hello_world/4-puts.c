6dca7beae045bb10f49db9b35c5a769d459d6d4f
DarkXerXerS <darkxerxers29@gmail.com>
2022-09-08 20:11:39 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <stdio.h>                                                                                                                                                        
-                                                                                                                                                                          
-/**                                                                                                                                                                       
-* main - print the string in the put function                                                                                                                             
-*                                                                                                                                                                         
-* Description: using the main function                                                                                                                                    
-*this program prints "programming is like building a multilingual puzzle                                                                                                  
-* Return: 0                                                                                                                                                               
-*/                                                                                                                                                                        
-int main(void)                                                                                                                                                            
-{                                                                                                                                                                         
-     put("\"programming is like building a multilingual puzzle");                                                                                                         
-     return(0);  
+#include <stdio.h>
+/**
+ *main -Function
+ *Return: 0 (Succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
