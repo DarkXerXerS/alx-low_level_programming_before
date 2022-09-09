ca1f2a4b8d515a0e61d80b7dce51bba7065cb739
DarkXerXerS <darkxerxers29@gmail.com>
2022-09-08 19:34:14 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-
+#include <stdio.h>
 /**
-* main - print the string in the function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multillingual puzzle
-* Return: 0
+* Description: main- Prints out a string to stdout.
+* Return: 0 if success.
 */
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
