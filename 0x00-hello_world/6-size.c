e2e24309bd3b07cc93e599431d0e1ee9762dad3b
DarkXerXerS <darkxerxers29@gmail.com>
2022-09-08 21:06:31 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,6 @@
 /**
  * main -print the string in the put function
  *
- * Description: using the main function
- * this program prints "programming is like a building mulitilingual puzzle
  * return: 0;
  */
  int main(void)
