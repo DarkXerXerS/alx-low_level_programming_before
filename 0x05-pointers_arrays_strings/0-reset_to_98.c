8478787884c8808b42549b18940a4edcb0abe817
XerXerS_d_coder <109603624+DarkXerXerS@users.noreply.github.com>
2022-09-20 18:10:19 +0100

    Delete 0-reset_to_98.c
    delete this file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * reset_to_98 - takes an int pointe and updates the value it points to to 98
- * @n: int pointer
- * Return: Always Void
-*/
-void reset_to_98(int *n)
-{
-    *n = 98;
